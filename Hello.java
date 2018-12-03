9db0ef55b9a2bc932a902f89dd5b131d543aece6
ade <ade.riyana@students.amikom.ac.id>
2018-12-03 17:00:10 +0700

    coba hapus

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main (String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello Word!");
 		
 		System.out.println("Sudah makan belum?")
